3ebabe6be0e3b55353a96c34889a586b9c681268
estaar <johnestaar@gmail.com>
2020-04-11 17:20:37 +0300

    Challages 1,2 3

--- src/estimator.py
+++ src/estimator.py
@@ -1,15 +1,17 @@
 def estimator(data):
-    dataset = datasets
-    if data["periodType"] == "months":
-        tilelapse = data["timeToElapse"] * 30
-    elif data["periodType"] == "weeks":
-        timelapse == data["timeToElapse"] * 7
-    else:
-        timelapse = data["timeToElapse"]
+    dataset = data
+    def timeLapse():
+        if data["periodType"] == "months":
+            timelapse = data["timeToElapse"] * 30
+        elif data["periodType"] == "weeks":
+            timelapse = data["timeToElapse"] * 7
+        else:
+            timelapse = data["timeToElapse"]
+        return timelapse
 
     def impacts():
         currentlyInfected = data["reportedCases"] * 10
-        infectionsByRequestedTime = currentlyInfected * (2 ** int((timelapse/ 3)))
+        infectionsByRequestedTime = currentlyInfected * (2 ** int((timeLapse()/ 3)))
         severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime
         hospitalBedsByRequestedTime = ((35 / 100) * data["totalHospitalBeds"]) - severeCasesByRequestedTime
         casesForICUByRequestedTime = (5 / 100) * infectionsByRequestedTime
@@ -28,10 +30,9 @@ def estimator(data):
                   }
 
         return impact
-
     def severeImpacts():
-        currentlyInfected = data["reportedCases"] * 50
-        infectionsByRequestedTime = currentlyInfected * (2 ** int((timelapse / 3)))
+        currentlyInfected = data["reportedCases"] * 10
+        infectionsByRequestedTime = currentlyInfected * (2 ** int((timeLapse()/ 3)))
         severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime
         hospitalBedsByRequestedTime = ((35 / 100) * data["totalHospitalBeds"]) - severeCasesByRequestedTime
         casesForICUByRequestedTime = (5 / 100) * infectionsByRequestedTime
@@ -39,16 +40,17 @@ def estimator(data):
         dollarsInFlight = (infectionsByRequestedTime * data["region"]['avgDailyIncomePopulation'] * data["region"][
           'avgDailyIncomeInUSD']) / data["timeToElapse"]
 
-        # Create severe impact dictionary
-        severeImpact = {"currentlyInfected": int(currentlyInfected),
-                        "infectionsByRequestedTime": int(infectionsByRequestedTime),
-                        "severeCasesByRequestedTime": int(severeCasesByRequestedTime),
-                        "hospitalBedsByRequestedTime": int(hospitalBedsByRequestedTime),
-                        "casesForICUByRequestedTime": int(casesForICUByRequestedTime),
-                        "casesForVentilatorsByRequestedTime": int(casesForVentilatorsByRequestedTime),
-                        "dollarsInFlight": int(dollarsInFlight),
+        # create impact dictionary
+        severeimpact = {"currentlyInfected": int(currentlyInfected),
+                  "infectionsByRequestedTime": int(infectionsByRequestedTime),
+                  "severeCasesByRequestedTime": int(severeCasesByRequestedTime),
+                  "hospitalBedsByRequestedTime": int(hospitalBedsByRequestedTime),
+                  "casesForICUByRequestedTime": int(casesForICUByRequestedTime),
+                  "casesForVentilatorsByRequestedTime": int(casesForVentilatorsByRequestedTime),
+                  "dollarsInFlight": int(dollarsInFlight),
+                  }
 
-                        }
-        return severeImpact
-    data = {"data": datasets, "impact": impacts(), "severeImpact": severeImpacts()}
+        return severeimpact
+
+    data = {"data": dataset, "impact": impacts(), "severeImpact": severeImpacts()}
     return data

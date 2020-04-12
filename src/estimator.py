c0511863bbf695f298bb89233aa429ce3d7e32ab
estaar <johnestaar@gmail.com>
2020-04-12 08:26:43 +0300

    Challages 1 review

--- src/estimator.py
+++ src/estimator.py
@@ -8,48 +8,49 @@ def estimator(data):
             timelapse = data["timeToElapse"] * 7
         else:
             timelapse = data["timeToElapse"]
-        timelapse = int(timelapse)
+        timelapse = int(timelapse / 3)
         return timelapse
+    # print(timeLapse())
 
     def impacts():
-        currentlyInfected = int(data["reportedCases"] * 10)
-        infectionsByRequestedTime = int(currentlyInfected * (2 ** (timeLapse()/ 3)))
-        severeCasesByRequestedTime = int((15 / 100) * infectionsByRequestedTime)
-        hospitalBedsByRequestedTime = int(((35 / 100) * data["totalHospitalBeds"]) - severeCasesByRequestedTime)
-        casesForICUByRequestedTime = int((5 / 100) * infectionsByRequestedTime)
-        casesForVentilatorsByRequestedTime = int((2 / 100) * infectionsByRequestedTime)
-        dollarsInFlight = int((infectionsByRequestedTime * data["region"]['avgDailyIncomePopulation'] * data["region"][
-          'avgDailyIncomeInUSD']) / data["timeToElapse"])
+        currentlyInfected = data["reportedCases"] * 10
+        infectionsByRequestedTime = currentlyInfected * (2 ** timeLapse())
+        # severeCasesByRequestedTime = int((15 / 100) * infectionsByRequestedTime)
+        # hospitalBedsByRequestedTime = int(((35 / 100) * data["totalHospitalBeds"]) - severeCasesByRequestedTime)
+        # casesForICUByRequestedTime = int((5 / 100) * infectionsByRequestedTime)
+        # casesForVentilatorsByRequestedTime = int((2 / 100) * infectionsByRequestedTime)
+        # dollarsInFlight = int((infectionsByRequestedTime * data["region"]['avgDailyIncomePopulation'] * data["region"][
+        #   'avgDailyIncomeInUSD']) / data["timeToElapse"])
 
         # create impact dictionary
         impact = {"currentlyInfected": int(currentlyInfected),
                   "infectionsByRequestedTime": int(infectionsByRequestedTime),
-                  "severeCasesByRequestedTime": int(severeCasesByRequestedTime),
-                  "hospitalBedsByRequestedTime": int(hospitalBedsByRequestedTime),
-                  "casesForICUByRequestedTime": int(casesForICUByRequestedTime),
-                  "casesForVentilatorsByRequestedTime": int(casesForVentilatorsByRequestedTime),
-                  "dollarsInFlight": int(dollarsInFlight),
+                  # "severeCasesByRequestedTime": int(severeCasesByRequestedTime),
+                  # "hospitalBedsByRequestedTime": int(hospitalBedsByRequestedTime),
+                  # "casesForICUByRequestedTime": int(casesForICUByRequestedTime),
+                  # "casesForVentilatorsByRequestedTime": int(casesForVentilatorsByRequestedTime),
+                  # "dollarsInFlight": int(dollarsInFlight),
                   }
 
         return impact
     def severeImpacts():
-        currentlyInfected = int(data["reportedCases"] * 10)
-        infectionsByRequestedTime = int(currentlyInfected * (2 ** int((timeLapse()/ 3))))
-        severeCasesByRequestedTime = int((15 / 100) * infectionsByRequestedTime)
-        hospitalBedsByRequestedTime = int(((35 / 100) * data["totalHospitalBeds"]) - severeCasesByRequestedTime)
-        casesForICUByRequestedTime = int((5 / 100) * infectionsByRequestedTime)
-        casesForVentilatorsByRequestedTime = int((2 / 100) * infectionsByRequestedTime)
-        dollarsInFlight = int((infectionsByRequestedTime * data["region"]['avgDailyIncomePopulation'] * data["region"][
-          'avgDailyIncomeInUSD']) / data["timeToElapse"])
+        currentlyInfected = data["reportedCases"] * 10
+        infectionsByRequestedTime = currentlyInfected * (2 ** timeLapse())
+        # severeCasesByRequestedTime = int((15 / 100) * infectionsByRequestedTime)
+        # hospitalBedsByRequestedTime = int(((35 / 100) * data["totalHospitalBeds"]) - severeCasesByRequestedTime)
+        # casesForICUByRequestedTime = int((5 / 100) * infectionsByRequestedTime)
+        # casesForVentilatorsByRequestedTime = int((2 / 100) * infectionsByRequestedTime)
+        # dollarsInFlight = int((infectionsByRequestedTime * data["region"]['avgDailyIncomePopulation'] * data["region"][
+        #   'avgDailyIncomeInUSD']) / data["timeToElapse"])
 
         # create impact dictionary
         severeimpact = {"currentlyInfected": int(currentlyInfected),
                   "infectionsByRequestedTime": int(infectionsByRequestedTime),
-                  "severeCasesByRequestedTime": int(severeCasesByRequestedTime),
-                  "hospitalBedsByRequestedTime": int(hospitalBedsByRequestedTime),
-                  "casesForICUByRequestedTime": int(casesForICUByRequestedTime),
-                  "casesForVentilatorsByRequestedTime": int(casesForVentilatorsByRequestedTime),
-                  "dollarsInFlight": int(dollarsInFlight),
+                  # "severeCasesByRequestedTime": int(severeCasesByRequestedTime),
+                  # "hospitalBedsByRequestedTime": int(hospitalBedsByRequestedTime),
+                  # "casesForICUByRequestedTime": int(casesForICUByRequestedTime),
+                  # "casesForVentilatorsByRequestedTime": int(casesForVentilatorsByRequestedTime),
+                  # "dollarsInFlight": int(dollarsInFlight),
                   }
 
         return severeimpact
@@ -61,6 +62,6 @@ def estimator(data):
 # with open("G:\Covid-19-SDG\Datas.json", 'r') as mydata:
 #   data = json.load(mydata)
 #
-# print(type(estimator("G:\Covid-19-SDG\Datas.json")))
+# print(estimator(data))
 
 # print(estimator(json.load("G:\Covid-19-SDG\Datas.json")))
